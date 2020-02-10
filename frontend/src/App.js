ef62dea5d9405cb3bbbdb2304822aaf410450de3
Hugh Bromund <hughbromund44@gmail.com>
2020-02-10 15:08:54 -0500

    Working on Navigation Bar + added new assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import classes from './App.module.css';
+import NavigationBar from './components/NavigationBar'
+
+
+/* This imports the react-bootstrap library */
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+
     <div className={classes.all}>
+      {/* This brings in the latest stylesheet for */}
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <NavigationBar></NavigationBar>
       <h1 className={classes.header}> FINEX </h1>
     </div>
+    
   );
 }
 
---
+++ frontend/src/components/NavigationBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
+export default class NavigationBar extends Component {
+    render() {
+        return (
+            <div>
+            <Navbar bg="dark" expand="lg">
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/../assets/img/logo-white.png"
+                        className="d-inline-block align-top"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="#home">TEST</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+            </div>
+        )
+    }
+}
