af62a49e7eb8aae1ddcc85353f2a7e6dff2b0ab3
Hugh Bromund <hughbromund44@gmail.com>
2020-02-11 12:44:27 -0500

    Navbar First Design
    -Implemented a Basic Navbar
    -Added a new assets folder for all website assets
    -Added a NavigationBar.css file to control the format of the Navbar

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import "./NavigationBar.css";
+
+
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
@@ -10,30 +13,38 @@ export default class NavigationBar extends Component {
     render() {
         return (
             <div>
-            <Navbar bg="dark" expand="lg">
+            <Navbar expand="lg" variant="dark" class="navbar-default">
                 <Navbar.Brand href="#home">
                     <img
-                        src="/../assets/img/logo-white.png"
+                        src={require('../assets/img/logo-white.png')}
                         className="d-inline-block align-top"
+                        width="150"
+                        alt="FINEX"
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                    <Nav.Link href="#home">TEST</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown title="STOCKS" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">YOUR STOCKS</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">FIND A STOCK</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">MARKET TRENDS</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
+                    <Nav.Link href="#link">FINANCE</Nav.Link>
+                    <NavDropdown title="BUDGETING" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">ADD A NEW ITEM</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">VIEW YOUR BUDGET</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">MAKE YOUR MONEY WORK</NavDropdown.Item>
+                    </NavDropdown>
+                    
                     </Nav>
-                    <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                    </Form>
+                        <Form inline>
+                            <Navbar.Text>
+                                SIGNED IN AS: <a href="#login">Hugh Bromund</a>
+                            </Navbar.Text>
+                        </Form>
                 </Navbar.Collapse>
             </Navbar>
             </div>
