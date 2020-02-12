b6dace9ce8bf9f11517e13ef47a4bebeae91b313
Peyton Williams <peytonwilliams5@gmail.com>
2020-02-12 11:24:10 -0500

    remove various prints

--- backend/Services/auto_complete.js
+++ backend/Services/auto_complete.js
@@ -1,33 +1,29 @@
 const fs = require('fs');
 
 const filepath = '../stock_list/nasdaq_list.csv'
+
+//parses nasdaq file into list
+//source: https://stackoverflow.com/questions/49616609/javascript-node-js-search-and-return-lines-that-contain-a-string-in-a-file
 const nasdaq_list = (fs.readFileSync(filepath))
     .toString()
     .replace(/['"]+/g, '')
     .split('\n')
     .map(line => line.split(',', 2))
     .map(line => Object.values(line));
-console.log(nasdaq_list);
 
-console.log(auto_complete("AAIT"));
-//parses nasdaq file into JSON data format
-//source: https://stackoverflow.com/questions/49616609/javascript-node-js-search-and-return-lines-that-contain-a-string-in-a-file
+console.log(auto_complete("bur"));
 
-
+//returns all matching stock symbols or names
 function auto_complete(search_term) {
-    let lines = nasdaq_list.filter(function (e) { 
-        console.log(typeof(e[1]));
-        if (typeof(e[0]) !== undefined && typeof(e[1]) !== undefined) {
-            return e[0].includes(search_term) || 
-            e[1].includes(search_term);
+    const search_lower = search_term.toLowerCase();
+
+    let lines = nasdaq_list.filter(function (e) {//filters all stocks which contain the searched term
+        if (e[1] !== undefined) { //fix by removing undefined from list
+            return e[0].toLowerCase().includes(search_lower) || 
+            e[1].toLowerCase().includes(search_lower);
         }
         return false;
-      
     });
-    
-    
-    
-    //let results = JSON.parse(lines);
 
     return lines;
 }
