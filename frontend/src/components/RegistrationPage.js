714fa9c75df423727bccdf54e71a8562ecee6673
Hugh Bromund <hughbromund44@gmail.com>
2020-02-12 16:19:49 -0500

    Added the FINEX logo to the registration page

--- frontend/src/components/RegistrationPage.js
+++ frontend/src/components/RegistrationPage.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
+import Image from 'react-bootstrap/Image'
 
 
 /*
@@ -10,6 +11,7 @@ import Card from 'react-bootstrap/Card'
  *
  * https://stackoverflow.com/questions/32282292/how-do-you-center-a-div-element-in-react-w-out-external-css-file/32282992
  * https://react-bootstrap.github.io/components/forms/
+ * https://react-bootstrap.github.io/components/images/
  * 
  */
 
@@ -18,6 +20,9 @@ export default class RegistrationPage extends Component {
         return (
             <div>
                 <h1>Welcome to FINEX, Let's get an account setup for you</h1>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Image src={require('../assets/img/logo-black.png')} style = {{ width: '50rem'}}fluid />
+                </div>
                 <div style={{display: 'flex', justifyContent: 'center'}}>
                         <Form style={{ width: '50rem'}}>
                             <Form.Group controlId="formBasicEmail">
