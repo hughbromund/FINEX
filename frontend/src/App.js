ff009d173b8775847d5218e63ff32ded8ca66238
Nathan Ashta <nathanashta@gmail.com>
2020-02-12 11:52:30 -0500

    prepared to merge to development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import classes from "./App.module.css";
-import ResetEmail from "./components/ResetEmail";
 
 function App() {
   return (
     <div className={classes.all}>
       <h1 className={classes.header}> FINEX </h1>
-      <ResetEmail></ResetEmail>
     </div>
   );
 }
--- frontend/src/components/ResetEmail.test.js
+++ frontend/src/components/ResetEmail.test.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ResetEmail from "./ResetEmail";
 
 const resetEmail = new ResetEmail();
