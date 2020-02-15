e859cbe10cba4df324e28fd374b6a7d8b32275b7
dadeej <joseph71@purdue.edu>
2020-02-15 16:22:21 -0500

    added router/service/controller

--- backend/Controllers/stock_controller.js
+++ backend/Controllers/stock_controller.js
@@ -16,8 +16,18 @@ exports.getHello = async function (req, res, next) {
      */
 }
 
-exports.get_auto_complete = async function (req, res, next) {
-    var stocks = await auto_complete_service.auto_complete(req.params.input); //TODO
-    return res.json(stocks);
+// function to validate, get, and return alpha vantage stock info
+exports.get_stock = async function (req, res, next) {
+    try {
+        let stock = await stockService.get_stock(req.params.code)
+        return res.json(stock);
+        //return res.status(200).json({ status: 200, data: stock, message: "Stock Retrieved" });
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
+}
 
+exports.get_auto_complete = async function (req, res, next) {
+    var stocks = await auto_complete_service.auto_complete(req.params.input);
+    return res.json(stocks);
 }
--- backend/Services/stock_service.js
+++ backend/Services/stock_service.js
@@ -1,9 +1,14 @@
 
 
+const alpha = require('alphavantage')({ key: '204HFII8A4KASQYO' });
+
 exports.HelloWorld = async function () {
     return 'Hello World';
 }
 
-exports.getUsers = async function (query, page, limit) {
-
+exports.get_stock = async function (code) {
+    alpha.data.intraday(code).then(data => {
+        console.log(data);
+        return data;
+    });
 }
