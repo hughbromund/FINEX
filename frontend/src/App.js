6a8e8338bb3b0b98b1390d734e2cb6ac56457b00
Aditya Naik <adityan9900@gmail.com>
2020-02-16 15:41:57 -0500

    Added favicon to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ResetEmail from "./components/ResetEmail";
 import Routes from "./routing/Routes";
 
-
 function App() {
   return (
     <div className={classes.all}>
       {/* This brings in the latest stylesheet for all bootstrap tools*/}
       <Routes />
-
     </div>
   );
 }
