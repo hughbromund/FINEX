7eaaa79475d2142891ae3b649179c3bca81c4449
niyatisriram <niyatisriram@gmail.com>
2020-02-16 14:30:13 -0500

    create method to insert new user

--- backend/mongo_commands.js
+++ backend/mongo_commands.js
@@ -6,12 +6,31 @@
 
 //define global variables
 var MongoClient = require('mongodb').MongoClient;
+
+mongo.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, (err, client) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+})
+
 var url = "mongodb+srv://niyatisriram:4A!B:mUj2_X8xZE@data-krx7s.mongodb.net/test?retryWrites=true&w=majority";
-var database = db.db("FINEX");
+var database = client.db("FINEX");
 var users = database.collection("Users");
 var transactions = database.collection("Transactions");
 
+
 //insert new user
+function insert_new_user(username, password, email, name) {
+  var new_user = { username: username, password: password, email: email, name: name };
+  database.insertOne(new_user, (err, result) => {
+    if (err) throw err;
+    console.log("New user inserted");
+  });
+}
 
 //check that username and email do not already exist in database
 
