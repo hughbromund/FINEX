a0c5b0f96ba4c5bef6019854f8dd1823fa5b5ae7
Peyton Williams <peytonwilliams5@gmail.com>
2020-03-08 14:29:29 -0400

    fixed naming and bug

---
+++ backend/Services/StockService.js
@@ -0,0 +1,19 @@
+const alpha = require('alphavantage')({ key: '204HFII8A4KASQYO' });
+
+exports.helloWorld = async function () {
+    return {"Hello": "World"};
+}
+
+//This has "realtime" data on a 1 minute interval
+exports.getStockIntraday = async function (code) {
+    return alpha.data.intraday(code).then(data => {
+        return data
+    });
+}
+
+//This returns data for each day going back as long as we want
+exports.getStockDaily = async function (code) {
+    return alpha.data.daily(code).then(data => {
+        return data
+    });
+}
