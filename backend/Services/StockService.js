664bb0e809cfa363c0b28f34bd748ff9a2e68ab8
dadeej <joseph71@purdue.edu>
2020-03-14 14:44:58 -0500

    added service functions

--- backend/Services/StockService.js
+++ backend/Services/StockService.js
@@ -30,7 +30,7 @@ exports.getSMA = async function (code, interval, series_type) {
         numInterval = "daily";
     }
 
-    return alpha.technical.sma(code, numInterval, 10, series_type).then(data => {
+    return alpha.technical.sma(code, numInterval, 50, series_type).then(data => {
         return alpha.util.polish(data).data;
     });
 }
@@ -44,7 +44,49 @@ exports.getEMA = async function (code, interval, series_type) {
         numInterval = "daily";
     }
 
-    return alpha.technical.ema(code, numInterval, 10, series_type).then(data => {
+    return alpha.technical.ema(code, numInterval, 50, series_type).then(data => {
+        return alpha.util.polish(data).data;
+    });
+}
+
+//This returns data for RSI (relative strength index) based on the interval provided
+exports.getRSI = async function (code, interval, series_type) {
+    let numInterval;
+    if (interval === "intraday") {
+        numInterval = "1min";
+    } else {
+        numInterval = "daily";
+    }
+
+    return alpha.technical.rsi(code, numInterval, 14, series_type).then(data => {
+        return alpha.util.polish(data).data;
+    });
+}
+
+//This returns data for bbands (Bollinger Bands) based on the interval provided
+exports.getBbands = async function (code, interval, series_type) {
+    let numInterval;
+    if (interval === "intraday") {
+        numInterval = "1min";
+    } else {
+        numInterval = "daily";
+    }
+
+    return alpha.technical.bbands(code, numInterval, 20, series_type, 2, 2).then(data => {
+        return alpha.util.polish(data).data;
+    });
+}
+
+//This returns data for EMA (moving average convergence / divergence) based on the interval provided
+exports.getMACD = async function (code, interval, series_type) {
+    let numInterval;
+    if (interval === "intraday") {
+        numInterval = "1min";
+    } else {
+        numInterval = "daily";
+    }
+
+    return alpha.technical.macd(code, numInterval, series_type, 12, 26, 9).then(data => {
         return alpha.util.polish(data).data;
     });
 }
