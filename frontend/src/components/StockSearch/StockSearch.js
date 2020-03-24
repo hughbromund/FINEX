48dbcdc60bc6364944851b530676cf63d5fc30c6
Aditya Naik <adityan9900@gmail.com>
2020-03-24 15:03:43 -0400

    Changed jumbotron styling

--- frontend/src/components/StockSearch/StockSearch.js
+++ frontend/src/components/StockSearch/StockSearch.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown'
-import FormControl from 'react-bootstrap/FormControl'
-import classes from './StockSearch.module.css'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Button from 'react-bootstrap/Button';
-import { STOCK_LIST_API } from '../../constants/Constants';
-import SearchBar from '../SearchBar/SearchBar';
-import {DarkModeContext, DarkModeProvider, useDarkModeState, useDarkModeToggle} from "../../contexts/DarkModeContext";
+import React, { Component } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import FormControl from "react-bootstrap/FormControl";
+import classes from "./StockSearch.module.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import { STOCK_LIST_API } from "../../constants/Constants";
+import SearchBar from "../SearchBar/SearchBar";
+import {
+  DarkModeContext,
+  DarkModeProvider,
+  useDarkModeState,
+  useDarkModeToggle
+} from "../../contexts/DarkModeContext";
 
 /**
  * This page is a page where the user may search for a stock.
@@ -14,24 +19,29 @@ import {DarkModeContext, DarkModeProvider, useDarkModeState, useDarkModeToggle} 
  * summary page.
  */
 class StockSearch extends Component {
-    render() {
-        return (
-            <div className={classes.wrapper}>
-                <div className={classes.innerDiv}>
-                    <Jumbotron className={this.context.isDarkMode ? classes.jumboDark : classes.jumboLight}>
-                            <h1>Welcome to FINEX's Stock Search Page!</h1>
-                            <p>
-                                Below, you may search for a stock by symbol or name. Clicking on the stock will bring you to its own, 
-                                dedicated screen where you can learn more about its recent trends.
-                            </p>
-                    </Jumbotron>
-                </div>
-                <div className={classes.innerDiv}>
-                    <SearchBar />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={classes.wrapper}>
+        <div className={classes.innerWrapper}>
+          <div className={classes.innerDiv}>
+            <Jumbotron
+              className={this.context.isDarkMode ? "bg-dark" : "bg-light"}
+            >
+              <h1>Welcome to FINEX's Stock Search Page!</h1>
+              <p>
+                Below, you may search for a stock by symbol or name. Clicking on
+                the stock will bring you to its own, dedicated screen where you
+                can learn more about its recent trends.
+              </p>
+            </Jumbotron>
+          </div>
+          <div className={classes.innerDiv}>
+            <SearchBar />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 StockSearch.contextType = DarkModeContext;
