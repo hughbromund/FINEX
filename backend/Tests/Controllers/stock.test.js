ef99cb29e2e2fc56206de53484ae73a29443d9ae
dadeej <joseph71@purdue.edu>
2020-03-24 19:34:31 -0500

    finished tests for daily and intraday

--- backend/Tests/Controllers/stock.test.js
+++ backend/Tests/Controllers/stock.test.js
@@ -42,13 +42,51 @@ describe("Stock Intraday", () => {
     })
 
     test("stock is not found", async () => {
+        const req = mockRequest('cscd')
+        const res = mockResponse()
+
+        stockService.getStockIntraday.mockImplementation(() => {
+            throw new Error();
+          });
+        await stockController.getStockIntraday(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ status: 400, message: 'cscd could not be found'})
+    })
+})
+
+
+describe("Stock Daily", () => {
+    test("expected path", async () => {
+        const req = mockRequest('msft')
+        const res = mockResponse()
+
+        const resp = {data: 'yeet'};
+        stockService.getStockDaily.mockResolvedValue(resp);
+        await stockController.getStockDaily(req, res)
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({data: 'yeet'})
+    })
+
+    test("code is undefined", async () => {
         const req = mockRequest(null)
         const res = mockResponse()
 
-        const resp = {status: 400};
-        stockService.getStockIntraday.mockResolvedValue(resp);
-        await stockController.getStockIntraday(req, res)
+        const resp = {data: 'yeet'};
+        stockService.getStockDaily.mockResolvedValue(resp);
+        await stockController.getStockDaily(req, res)
         expect(res.status).toHaveBeenCalledWith(400)
         expect(res.json).toHaveBeenCalledWith({ status: 400, message: 'code is undefined'})
+    })
+
+    test("stock is not found", async () => {
+        const req = mockRequest('cscd')
+        const res = mockResponse()
+
+        stockService.getStockDaily.mockImplementation(() => {
+            throw new Error();
+          });
+        await stockController.getStockDaily(req, res)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith({ status: 400, message: 'cscd could not be found'})
     })
 })
