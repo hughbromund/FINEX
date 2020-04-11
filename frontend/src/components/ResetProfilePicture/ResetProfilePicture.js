e2e3f4a07538bea9b8a15c72222a027ec41924d4
Hugh Bromund <hughbromund44@gmail.com>
2020-04-11 13:37:30 -0400

    basic drag and drop selection is working

--- frontend/src/components/ResetProfilePicture/ResetProfilePicture.js
+++ frontend/src/components/ResetProfilePicture/ResetProfilePicture.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import Dropzone from "react-dropzone";
 
 import classes from "./ResetProfilePicture.module.css";
 
+const imageMaxSize = 1000;
+const acceptedFileTypes = ["png", "jpg"];
+
 export default class ResetProfilePicture extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+  }
   render() {
     return (
       <div className={classes.wrapper}>
         <div className={classes.inner}>
           <h1>Update Profile Picture</h1>
+          <Dropzone
+            onDrop={(acceptedFiles) => {
+              // console.log(acceptedFiles);
+              this.setState({
+                file: URL.createObjectURL(acceptedFiles[0]),
+              });
+            }}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+          <img src={this.state.file} />
         </div>
       </div>
     );
