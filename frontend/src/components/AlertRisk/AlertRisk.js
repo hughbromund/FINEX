c567816661c376042da453ec47d5bfa543ba2699
Nathan Ashta <nathanashta@gmail.com>
2020-04-21 12:30:20 -0500

    added padding in risk management

--- frontend/src/components/AlertRisk/AlertRisk.js
+++ frontend/src/components/AlertRisk/AlertRisk.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Container, Row, Col, Alert, Button } from "react-bootstrap";
+import { Container, Row, Col, Alert, Button, Jumbotron } from "react-bootstrap";
 import history from "../../routing/History";
-import { INVESTMENT_TACTICS_PATH } from "../../constants/Constants";
+import {
+  INVESTMENT_TACTICS_PATH,
+  RISK_MANAGEMENT_PATH,
+} from "../../constants/Constants";
 import classes from "./AlertRisk.module.css";
 
 export default class AlertRisk extends Component {
@@ -10,19 +13,24 @@ export default class AlertRisk extends Component {
       <div>
         <Container>
           <Row>
-            <Col className={classes.mycol}></Col>
-          </Row>
-          <Row>
-            <Col>
+            <Col className={classes.mycol}>
               <div className={classes.cont}>
                 <Alert variant={"danger"} className={classes.alert}>
+                  <h1>FINEX OFFICIAL DISCLAIMER</h1>
                   By moving on to our investment page, you acknowledge that you
                   understand the risks of investing and that you do not hold
                   FINEX or its developers liable for any losses incurred by
                   advice given in this website. We are not a financial advisor
                   and do not attempt to be.
-                  <br />
-                  <br />
+                  <hr />
+                  <Button
+                    variant={"danger"}
+                    onClick={() => history.push(RISK_MANAGEMENT_PATH)}
+                  >
+                    I would like to learn more about the risks involved in
+                    investing.
+                  </Button>
+                  &nbsp; &nbsp; &nbsp;
                   <Button
                     variant={"success"}
                     onClick={() => history.push(INVESTMENT_TACTICS_PATH)}
